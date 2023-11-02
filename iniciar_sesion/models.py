ea91e64141ce06560ff91509c0dd1431549622a6
MiguelPeralta123 <maikperalta123@gmail.com>
2023-11-02 12:43:07 -0700

    Blocking table along with other fields

--- iniciar_sesion/models.py
+++ iniciar_sesion/models.py
@@ -21,7 +21,6 @@ class CustomUserManager(UserManager):
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
     is_staff = models.BooleanField(default=False)
